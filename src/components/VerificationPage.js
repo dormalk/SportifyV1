01aecae9538566c67ac98d00b2911ad07e9bf63b
elaya38 <dormalk@gmail.com>
2018-08-10 12:22:28 +0300

    styling verification page

--- src/components/VerificationPage.js
+++ src/components/VerificationPage.js
@@ -6,8 +6,16 @@ import { sendEmailVerification } from '../actions/auth';
 
 export const VerificationPage = ({sendEmailVerification}) => (
     <div>
-        <p>חשבון לא מאומת</p>
-        <button onClick={sendEmailVerification}>שלח אימות במייל</button>
+        <div className="content-container">
+            <div className="verification_box">
+                <h3>חשבון לא מאומת</h3>
+                <p>
+                חשבונך עדין לא אומת מול המערכת, במידה ולא קיבלת מייל אימות, באפשרותך לבצע שליחה חוזרת של המייל.<br/>
+                תהליך זה הכרחי על-מנת למטב את המערכת וליצור עבורך כלי איכותי לצרכייך.<br/> 
+                </p>
+                <button onClick={sendEmailVerification}>שלח אימות במייל</button>        
+            </div>
+        </div>
     </div>
 ); 
 
