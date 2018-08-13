156b98bc113434053d9417c2f7c7980e520baa85
elaya38 <dormalk@gmail.com>
2018-08-13 23:01:57 +0300

    add LetMeKnowYouPage page and transition on components

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { login, logout } from './actions/auth';
+import { startSetUser } from './actions/user';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import { firebase } from './firebase/firebase';
@@ -28,6 +29,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     store.dispatch(login(user.uid));
+    store.dispatch(startSetUser());
     renderApp();
     if (history.location.pathname === '/') {
       history.push('/dashboard');
--- src/components/LetMeKnowYouMorePage.js
+++ src/components/LetMeKnowYouMorePage.js
@@ -1,7 +1,101 @@
 //Test
 import React from 'react';
-export default () =>(
+import { connect } from 'react-redux';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+const YourSex = ({hendelSexChange}) => (
     <div>
-        <h1>אני רוצה להכיר אותך יותר</h1>
+        <h2>מהו מינך?</h2>
+        <form>
+            <label>
+                זכר
+                <input 
+                    onChange={hendelSexChange}
+                    type='radio'
+                    name='sex'
+                    value='זכר'/>
+            </label>
+            <label>
+                נקבה
+                <input
+                    onChange={hendelSexChange} 
+                    type='radio' 
+                    name='sex'
+                    value='נקבה'/>
+            </label>
+            <label>
+                לא מעוניין למסור
+                <input 
+                    onChange={hendelSexChange}
+                    type='radio'
+                    name='sex' 
+                    value='ללא'/>
+            </label>
+        </form>
+    </div>
+);
+
+const YourAge = ({hendelAgeChange}) => (
+    <div>
+        <h2>גיל</h2>
+        <input type="number" placeholder="18" min="16" max="99" onChange={hendelAgeChange}/>
+    </div>
+);
+
+const YourHobbies = () => (
+    <div>
+        <h2>מה התחביבים שלך?</h2>
     </div>
 )
+
+export class LetMeKnowYouMorePage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: this.props.name || '',
+            sex: '',
+            age:''
+        }
+
+    }
+
+    hendelSexChange = (e) => {
+        const sex = e.target.value;
+        this.setState({sex});
+    }
+
+    hendelAgeChange = (e) => {
+        const age = e.target.value;
+        this.setState({age});
+    }
+
+    render(){
+        return(
+            <div className="content-container">
+                <div>
+                    {this.props.name && <h1>היי  {this.props.name}, אני רוצה להכיר אותך יותר!</h1>}
+                    <ReactCSSTransitionGroup
+                        transitionName="exm"
+                        transitionAppear={false}
+                        transitionEnterTimeout={500}
+                        transitionEnter={true}
+                        transitionLeave={false}>
+                    <div>{this.props.name &&
+                        <YourSex
+                            hendelSexChange={this.hendelSexChange}
+                        />}
+                    </div>
+                    {this.state.sex && <YourAge hendelAgeChange={this.hendelAgeChange}/>}
+                    {this.state.age && <YourHobbies/>}
+                    </ReactCSSTransitionGroup>
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state, props) => ({
+    name: state.user.fname
+});
+
+export default connect(mapStateToProps,undefined)(LetMeKnowYouMorePage);
